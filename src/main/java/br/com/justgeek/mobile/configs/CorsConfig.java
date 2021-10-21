18a79bee3b7c5c89d6ab54f57c5accc0c0216777
GabrielSantosGM <gabriel.sgomes@bandtec.com.br>
2021-10-21 17:10:14 -0300

    cors brabo

--- src/main/java/br/com/justgeek/mobile/configs/CorsConfig.java
+++ src/main/java/br/com/justgeek/mobile/configs/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
